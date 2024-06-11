bdfd1a6ea9fa706746de90cf2c41f22b746de728
TruongBuuTue <117697961+TruongBuuTue@users.noreply.github.com>
2024-06-11 09:02:14 +0700

    Update bai01.js

--- JS/bai01.js
+++ JS/bai01.js
@@ -1,13 +1,3 @@
-// Tên file: bai01.js
+// Tên file: helloWorld.js
 
-function daoNguocChuoi(chuoi) {
-  // Sử dụng phương thức split để chia chuỗi thành mảng các ký tự
-  // Sau đó sử dụng phương thức reverse để đảo ngược mảng
-  // Cuối cùng sử dụng phương thức join để nối các ký tự thành chuỗi
-  return chuoi.split('').reverse().join('');
-}
-
-// Ví dụ sử dụng
-console.log(daoNguocChuoi("hello")); // Kết quả: "olleh"
-console.log(daoNguocChuoi("abcde")); // Kết quả: "edcba"
-console.log(daoNguocChuoi("12345")); // Kết quả: "54321"
+console.log('Hello, World!');
