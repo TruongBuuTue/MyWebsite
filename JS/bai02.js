3e58db5befb0076895cdde05f3123aeff2224b97
TruongBuuTue <117697961+TruongBuuTue@users.noreply.github.com>
2024-06-15 00:23:23 +0700

    Update bai02.js

--- JS/bai02.js
+++ JS/bai02.js
@@ -1,22 +1,12 @@
-
-// Hàm tính số Fibonacci sử dụng phương pháp lặp
 function fibonacci(n) {
-  if (n <= 1) {
-    return n;
-  }
-
-  let a = 0, b = 1, temp;
-  for (let i = 2; i <= n; i++) {
-    temp = a + b;
-    a = b;
-    b = temp;
-  }
-  return b;
+    if (n <= 1) return n;
+    let fibPrev = 0;
+    let fibCurr = 1;
+    for (let i = 2; i <= n; i++) 
+       {
+        let temp = fibCurr;
+        fibCurr += fibPrev;
+        fibPrev = temp;
+    }
+    return fibCurr;
 }
-
-// Ví dụ sử dụng
-console.log(fibonacci(5));  // Kết quả: 5
-console.log(fibonacci(10)); // Kết quả: 55
-console.log(fibonacci(0));  // Kết quả: 0
-console.log(fibonacci(1));  // Kết quả: 1
-console.log(fibonacci(20)); // Kết quả: 6765
